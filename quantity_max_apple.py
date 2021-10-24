a232ea78e985708e6da6930beede4835b190233e
jendlrs <jensennics.dlrs@gmail.com>
2021-10-24 19:23:12 +0800

    code completed

--- quantity_max_apple.py
+++ quantity_max_apple.py
@@ -1,8 +1,7 @@
-money= int(input("\n Enter the amount of money you have: \n>"))
-cost_per_apple= int(input("\n Price per apple: \n>"))
+money= float(input("\n Enter the amount of money you have: \n> "))
+cost_per_apple= float(input("\n Price per apple: \n> "))
 
-max_quantity= money//cost_per_apple
-total= max_quantity*cost_per_apple
-change= money-total
+max_quantity= int(money/cost_per_apple)
+change= money % cost_per_apple
 
-print(f"\n You can buy {max_quantity} apples and your change is {change}. \n")
+print(f"\n You can buy {max_quantity} apples and your change is {change:.2f} pesos. \n")
