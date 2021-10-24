d335207f7fbb152294f3896538b40228692361dc
jendlrs <jensennics.dlrs@gmail.com>
2021-10-24 18:06:45 +0800

    code completed

--- apples_and_oranges.py
+++ apples_and_oranges.py
@@ -3,3 +3,7 @@ quantity_oranges= int(input("\n How many oranges do you want to buy? \n>"))
 
 cost_per_apple= 20
 cost_per_orange= 25
+
+total= cost_per_apple*quantity_apples + cost_per_orange*quantity_oranges
+
+print(f"\n The total amount is {total}. \n")
