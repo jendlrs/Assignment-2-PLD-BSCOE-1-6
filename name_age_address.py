a8a8aef627b90657a9ab9eb81665750bf45a7f22
jendlrs <jensennics.dlrs@gmail.com>
2021-10-24 17:42:45 +0800

    code completed

--- name_age_address.py
+++ name_age_address.py
@@ -1,3 +1,5 @@
 name= input("\n What is your name? \n> ")
 age= input("\n How old are you? \n> ")
 address= input("\n Where do you live? \n> ")
+
+print(f"\n Hi, my name is {name}. I am {age} years old and I live in {address}. \n")
